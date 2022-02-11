51ad08fdc6c634b644359e9ae06df8eb236898b6
Joshua Cachola <joshuaelliscachola@gmail.com>
2022-02-11 01:56:00 -0800

    first draft of card complete

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,22 +1,31 @@
 import { View, StyleSheet, Text } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 export default function Card() {
   return (
     <View style={styles.container}>
-      <Text style={styles.value}>
-        K
-        <FontAwesomeIcon height={28} width={28} icon={faHeart} color={"red"} />
-      </Text>
-      <Text
-        style={[
-          styles.value,
-          styles.bottomValue,
-          { transform: [{ rotateX: "180deg" }] },
-        ]}
-      >
-        K
-        <FontAwesomeIcon height={28} width={28} icon={faHeart} color={"red"} />
+      <Text style={styles.face}>
+        <Text style={styles.value}>
+          K
+          <FontAwesomeIcon
+            height={28}
+            width={28}
+            icon={faHeart}
+            color={"red"}
+          />
+        </Text>
+        <Text style={[styles.value, styles.bottomValue]}>
+          K
+          <Text>
+            <FontAwesomeIcon
+              height={28}
+              width={28}
+              icon={faHeart}
+              color={"red"}
+            />
+          </Text>
+        </Text>
       </Text>
     </View>
   );
@@ -30,12 +39,26 @@ const styles = StyleSheet.create({
     borderColor: "#C5C5C5",
     borderWidth: 1,
   },
+  face: {
+    height: "100%",
+  },
   value: {
     fontWeight: "bold",
     fontSize: 30,
     color: "red",
     display: "flex",
     flexDirection: "column",
+    height: "50%",
+    marginHorizontal: 8,
   },
-  bottomValue: {},
+  bottomValue: {
+    transform: [
+      {
+        rotateX: "180deg",
+      },
+    ],
+    alignContent: "flex-end",
+    alignItems: "flex-end",
+    flexBasis: "auto",
+  },
 });
