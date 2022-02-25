045d6460fd711bf2fee7dcc4bbd82527bf012272
Joshua Cachola <joshuaelliscachola@gmail.com>
2022-02-24 17:25:59 -0800

    add: GameState initial class

--- classes/GameState.ts
+++ classes/GameState.ts
@@ -1,3 +1,6 @@
+import { CardInterface } from './Card';
+import { Deck } from './Deck';
+
 type combination = 'SINGLE' | 'DOUBLE' | 'TRIPLE' | 'STRAIGHT' | null;
 
 export interface GameInterface {
@@ -15,21 +18,72 @@ export class GameState implements GameInterface {
     length: number;
   };
   players: string[];
+  playerRotation: string[];
   roomId: string;
   isGameWon: boolean;
+  ableToStartGame: boolean;
+  currentPlayer: string | null;
+  hands: CardInterface[][];
 
   constructor(roomId: string) {
     this.combination = {
       type: null,
       length: 0,
     };
-    this.players = [];
+    this.players = ['player 1', 'player 2', 'player 3', 'player 4'];
     this.roomId = roomId;
     this.isGameWon = false;
+    this.ableToStartGame = false;
+    this.currentPlayer = null;
+    this.playerRotation = [];
+    this.hands = [];
   }
 
   // change to player object
   addPlayer(player: string) {
     this.players.push(player);
+  }
+
+  // checks to see if there are enough players to start a game
+  setAbleToStartGame() {
+    if (this.players.length > 1) {
+      return true;
+    }
+    return false;
+  }
+
+  // creates the rotation of players
+  createPlayerRotation(startingPlayerIdx: number) {
+    const rotation: string[] = [this.players[startingPlayerIdx]];
+    for (
+      let i = startingPlayerIdx + 1;
+      i % this.players.length !== startingPlayerIdx;
+      i++
+    ) {
+      rotation.push(this.players[i]);
+    }
+    return rotation.reverse();
+  }
+
+  deal(deck: CardInterface[]) {
+    const hands: CardInterface[][] = [[], [], [], []];
+    deck.forEach((card: CardInterface) => {
+      hands[0].push(card);
+      hands[1].push(card);
+      hands[2].push(card);
+      hands[3].push(card);
+    });
+    return hands;
+  }
+
+  // starts the game and sets
+  start() {
+    if (!this.ableToStartGame) {
+      return false;
+    }
+
+    const startingIdx = Math.random() * this.players.length;
+    this.playerRotation = this.createPlayerRotation(startingIdx);
+    this.currentPlayer = this.playerRotation[this.playerRotation.length - 1];
   }
 }
