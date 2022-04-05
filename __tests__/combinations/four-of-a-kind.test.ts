85b9a354f927bf6f4b5767f6d87824a155f77ac5
Joshua Cachola <joshuaelliscachola@gmail.com>
2022-04-04 20:17:15 -0700

    add: new triple and four of a kind / bomb tests

--- __tests__/combinations/four-of-a-kind.test.ts
+++ __tests__/combinations/four-of-a-kind.test.ts
@@ -1,22 +1,71 @@
 import { describe, test, expect } from '@jest/globals';
-import { CardType } from '../../classes/Card';
+import { Card, CardType } from '../../classes/Card';
 import { combinationConstants } from '../../constants/CombinationConstants';
 import { isValidCombination } from '../../helper/combinationHelpers';
 
 describe('four-of-a-kind bombs', () => {
   describe('checks for valid four-of-a-kind', () => {
-    test('four nines returns true', () => {});
+    test('four nines returns true', () => {
+      const incoming: CardType[] = [
+        new Card(0, 9),
+        new Card(1, 9),
+        new Card(2, 9),
+        new Card(3, 9),
+      ];
+      const [isValid, type] = isValidCombination(incoming, null, null);
+      expect(isValid).toBeTruthy();
+      expect(type).toBe('BOMB');
+    });
   });
 
   describe('checks for invalid four-of-a-kind', () => {
-    test('three nines and a 10 returns false', () => {});
+    test('three nines and a 10 returns false', () => {
+      const incoming: CardType[] = [
+        new Card(0, 9),
+        new Card(1, 9),
+        new Card(2, 9),
+        new Card(3, 10),
+      ];
+      const [isValid, _] = isValidCombination(incoming, null, null);
+      expect(isValid).toBeFalsy();
+    });
   });
 
   describe('checks for incoming four-of-a-kind beats current', () => {
-    test('four nines beats four eights', () => {});
+    test('four nines beats four eights', () => {
+      const incoming: CardType[] = [
+        new Card(0, 9),
+        new Card(1, 9),
+        new Card(2, 9),
+        new Card(3, 9),
+      ];
+      const current = new Card(3, 8);
+      const [isValid, _] = isValidCombination(
+        incoming,
+        combinationConstants.BOMB,
+        current,
+        4
+      );
+      expect(isValid).toBeTruthy();
+    });
   });
 
   describe('checks for incoming four-of-a-kind does not beat current', () => {
-    test('four nines does not beat four tens', () => {});
+    test('four nines does not beat four tens', () => {
+      const incoming: CardType[] = [
+        new Card(0, 9),
+        new Card(1, 9),
+        new Card(2, 9),
+        new Card(3, 9),
+      ];
+      const current = new Card(3, 10);
+      const [isValid, _] = isValidCombination(
+        incoming,
+        combinationConstants.BOMB,
+        current,
+        4
+      );
+      expect(isValid).toBeFalsy();
+    });
   });
 });
--- __tests__/combinations/triple.test.ts
+++ __tests__/combinations/triple.test.ts
@@ -1,23 +1,68 @@
 import { describe, test, expect } from '@jest/globals';
-import { CardType } from '../../classes/Card';
+import { Card, CardType } from '../../classes/Card';
 import { combinationConstants } from '../../constants/CombinationConstants';
 import { isValidCombination } from '../../helper/combinationHelpers';
 
 // triples
 describe('triple combinations', () => {
   describe('checks for valid triple combination by value of card', () => {
-    test('triple fives beat triple fours', () => {});
+    test('triple fives beat triple fours', () => {
+      const incoming: CardType[] = [
+        new Card(0, 5),
+        new Card(1, 5),
+        new Card(2, 5),
+      ];
+      const current = new Card(2, 4);
+      const [isValid, _] = isValidCombination(
+        incoming,
+        combinationConstants.TRIPLE,
+        current,
+        3
+      );
+      expect(isValid).toBeTruthy();
+    });
   });
 
   describe('checks for invalid triple combinations by value of card', () => {
-    test('triple fours does not beat triple fives', () => {});
-  });
-
-  describe('checks for ', () => {
-    test('triple fours does not beat triple fives', () => {});
+    test('triple fours does not beat triple fives', () => {
+      const incoming: CardType[] = [
+        new Card(0, 4),
+        new Card(1, 4),
+        new Card(2, 4),
+      ];
+      const current = new Card(2, 5);
+      const [isValid, _] = isValidCombination(
+        incoming,
+        combinationConstants.TRIPLE,
+        current,
+        3
+      );
+      expect(isValid).toBeFalsy();
+    });
   });
 
   describe('checks for triple with invalid triple', () => {
-    test('2 fours and 1 five returns false', () => {});
+    test('2 fours and 1 five returns false', () => {
+      const incoming: CardType[] = [
+        new Card(0, 4),
+        new Card(1, 4),
+        new Card(2, 5),
+      ];
+      const [isValid, _] = isValidCombination(incoming, null, null);
+      expect(isValid).toBeFalsy();
+    });
+  });
+
+  describe('identifies valid triple', () => {
+    test('3 fours returns valid and "TRIPLE" combination type', () => {
+      const incoming: CardType[] = [
+        new Card(0, 4),
+        new Card(1, 4),
+        new Card(2, 4),
+      ];
+      const [isValid, type] = isValidCombination(incoming, null, null);
+      expect(isValid).toBeTruthy();
+      expect(type).toBe('TRIPLE');
+    });
   });
 });
